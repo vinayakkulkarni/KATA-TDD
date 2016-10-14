186f37c4a4676200d08e96dcaff8bb63493c7799
Vinayak Kulkarni <vinayak@shoptreeapp.com>
2016-10-14 17:39:31 +0530

    Added example for better understanding the use cases.

--- src/GCF.php
+++ src/GCF.php
@@ -22,3 +22,23 @@ class GCF
 
 	}
 }
+
+/* 
+Example : 18 - 24
+num1 = 18 ; num2 = 24
+$tmp = 18%24 = 18
+
+    while tmp != 0
+    num1 holds value of num2 = 24
+    num2 = tmp = 18
+    new tmp = 24%18 = 6
+
+    again while tmp != 0
+    num1 = 24
+    num2 = tmp = 6
+    new tmp = 24%6 = 0
+
+    now tmp = 0; exits while loop
+
+$num2 has value of 6 which is GCF of 18 & 24
+*/
--- src/PerfectNumber.php
+++ src/PerfectNumber.php
@@ -25,3 +25,24 @@ class PerfectNumber
         }
     }
 }
+
+/* Example: 6
+$number = 6
+$candidate = 2
+
+ if ((num)6 % (candidate)2 == 0) {
+
+    $perfect = array(1);
+
+    for (sum = 2; (sum)2 < (num)6; 2++){
+
+        if ((num)6 % (sum)2 == 0) {
+            add the value of $sum to perfect[] array
+        }
+
+    }
+
+    if sum of elements in perfect[] array  == $number
+        return $perfect;
+ }
+*/
